a656646daad24e576279a0a4a38075c87fd6e412
taiga <taiga@J16017>
2018-07-12 11:16:03 +0900

    遅れです

--- src/recognition01/Recognition01_main.java
+++ src/recognition01/Recognition01_main.java
@@ -2,7 +2,10 @@ package recognition01;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectFacesOptions;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
@@ -10,7 +13,7 @@ import com.ibm.watson.developer_cloud.visual_recognition.v3.model.DetectedFaces;
 public class Recognition01_main {
 	public static void main(String[]orgs){
 	VisualRecognition service = new VisualRecognition("2018-03-19");
-	service.setApiKey("1b184fdfa1b1641d657f68330ab436b8aa7097e9");
+	service.setApiKey("J16017");
 
 	DetectFacesOptions detectFacesOptions = null;
 	try {
@@ -23,5 +26,20 @@ public class Recognition01_main {
 	}
 	DetectedFaces result = service.detectFaces(detectFacesOptions).execute();
 	System.out.println(result);
-	}
+	String s=String.valueOf(result);
+
+	ObjectMapper mapper=new ObjectMapper();
+	JsonNode node=null;
+
+		try {
+			node=mapper.readTree(result.toString());
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	int age_min=
+			node.get("images").get(0).get("faces").get(0).get("age").get("min").asInt();
+			System.out.println("age_min:"+age_min);
+}
 }
